c63c37775f86a66251d8e37e9ae91145048073e0
Diego <luarca84@gmail.com>
2016-10-11 19:12:33 +0200

    First Commit

---
+++ Luarca/app/src/main/java/com/luarca84/diego/luarca/MainActivity.java
@@ -0,0 +1,13 @@
+package com.luarca84.diego.luarca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
